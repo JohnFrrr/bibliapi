5c70b18c792592af4f4416f5afcec44a3dbfaa5c
João Ferreira <joaov.ssff@gmail.com>
2024-10-07 22:10:59 -0300

    Trying to populate redb database

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,39 @@
-pub mod models;
-pub mod schema;
+use redb::{Database, TableDefinition};
 
-use bytes::Bytes;
-use futures_util::StreamExt;
+use csv::ReaderBuilder; // Import redb::Error
+use std::error::Error;
+use std::fs::File; // Import csv::Error
+                   // Define a custom error enum to encapsulate both error types
+                   // Define a custom error enum to encapsulate multiple error types
 
-use axum::{extract::BodyStream, routing::get, routing::post, Router};
-use tokio::net::TcpListener;
+const VERSES: TableDefinition<(u8, u8, u8), &str> = TableDefinition::new("VERSES");
 
-use diesel::prelude::*;
-use dotenvy::dotenv;
-use std::env;
-
-#[tokio::main(flavor = "current_thread")]
-async fn main() {
-    // build our application with a route
-    let app = Router::new()
-        .route("/", get(help))
-        .route("/echo", post(echo))
-        .route("/post", get(get_posts));
-
-    // run it
-    let addr = "0.0.0.0:8080";
-    let tcp_listener = TcpListener::bind(addr).await.unwrap();
-    println!("listening on {}", addr);
-    axum::Server::from_tcp(tcp_listener.into_std().unwrap())
-        .unwrap()
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
-
-async fn help() -> &'static str {
-    "Try POSTing data to /echo such as: `curl localhost:8080/echo -XPOST -d 'hello world'`\n"
-}
-
-async fn echo(mut stream: BodyStream) -> Bytes {
-    if let Some(Ok(s)) = stream.next().await {
-        s
-    } else {
-        Bytes::new()
+fn main() -> Result<(), Box<dyn Error>> {
+    // Open the CSV file
+    let file = File::open("db/VERSES.csv")?;
+    // Create a CSV reader
+    let mut rdr = ReaderBuilder::new()
+        .has_headers(false) // Specify if the file has headers
+        .from_reader(file);
+    let db = Database::create("db/biblia.redb")?;
+    let write_txn = db.begin_write()?;
+    {
+        let mut table = write_txn.open_table(VERSES)?;
+        for result in rdr.records() {
+            let record = result?;
+            let book: u8 = record[0].parse().unwrap();
+            let chapter: u8 = record[1].parse().unwrap();
+            let verse: u8 = record[2].parse().unwrap();
+            table.insert((book, chapter, verse), &record[3])?;
+        }
     }
-}
+    // Iterate over each record
 
-pub fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
+    write_txn.commit()?;
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
-}
+    let read_txn = db.begin_read()?;
+    let table = read_txn.open_table(VERSES)?;
+    println!("{}", table.get((1, 1, 1))?.unwrap().value());
 
-async fn get_posts() -> Result<String, String> {
-    use self::schema::posts::dsl::*;
-
-    let connection = &mut establish_connection();
-    let results = posts
-        .filter(published.eq(true))
-        .limit(5)
-        .select(models::Post::as_select())
-        .load(connection)
-        .expect("Error loading posts");
-
-    let mut result = String::new();
-
-    println!("Displaying {} posts", results.len());
-    for post in results {
-        result.push_str(&format!("{}\n", post.title));
-        result.push_str("-----------\n");
-        result.push_str(&format!("{}", post.body));
-        result.push('\n');
-    }
-
-    Ok(result)
+    Ok(())
 }
